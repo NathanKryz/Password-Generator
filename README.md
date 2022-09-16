e9849e86b2c72e0174ec7275a6b370340e5420da
NathanKryz <112029666+NathanKryz@users.noreply.github.com>
2022-09-16 17:41:39 -0400

    Final script updates, added comments, filled out README

---
+++ README.md
@@ -0,0 +1,20 @@
+# Javascript Password Generator
+
+## Description
+
+The following repository contains all the code needed to create a fully functional password generator program in a webbrowser page. Through the power of javascript, users will be able to be walked through the steps to generate a random password, using criteria of their own desire, so that they may have a strong password down the line.
+
+## Installation
+
+No installation required, code runs fully within the browser, or launched from the index.html locally.
+
+## Usage
+
+    By opening the webpage and pressing the large "Generate" button, a series of prompts will occur, asking for length of a password, whether or not to include lowercase, UPPERCASE, numbers or special characters. With valid input, a randomly generated password will appear in the box on screen. Invalid inputs will be caught by the program and the user will be prompted to either correct the mistake, or should they choose, to terminate the program so users are not trapped in a forced loop.
+
+    Link:
+
+    Screenshot of the repo in use:
+    
+    ![Screenshot of the randomly generated password](password-generator-screenshot.png)
+    
--- script.js
+++ script.js
@@ -1,6 +1,6 @@
-// Assignment Code
+// Creates a button to be used for the generation !Untouched!
 var generateBtn = document.querySelector("#generate");
-// Newly added code below
+// Declaring the many needed variables.
 let passwordlength;
 let allowlowercase;
 let allowuppercase;
@@ -10,71 +10,70 @@ let chosencharacters;
 let validchoice;
 let newpassword;
 
-//let passlowercase = ["a","b","c","d","e","f","g","h"]
 //Generating arrays containing all the possible inputs for each character category
 let uppercase = Array.from(Array(26)).map((e, i) => i + 65);
 let passuppercase = uppercase.map((x) => String.fromCharCode(x));
-console.log(passuppercase);
 
 let lowercase = Array.from(Array(26)).map((e, i) => i + 97);
 let passlowercase = lowercase.map((x) => String.fromCharCode(x));
-console.log(passlowercase);
 
 let numbercase = Array.from(Array(10)).map((e, i) => i + 48);
 let passnumbers = numbercase.map((x) => String.fromCharCode(x));
-console.log(passnumbers);
 //This one can't be done the same way as the char codes are all over the place!
 let passspecial = [" ","!","\"","#","$","%","&","\'","(",")","*","+",",","-",".","/",":",";","<","=",">","?","@","[","\\","]","^","_","`","{","|","}","~"];
-console.log(passspecial);
 
-// Write password to the #password input
+// Write password to the #password input !Untouched!
 function writePassword() {
   var password = generatePassword();
   var passwordText = document.querySelector("#password");
 
   passwordText.value = password;
 }
-
+// Password generating function, starts with sanitizing variables
 function generatePassword(){
   chosencharacters = [];
   newpassword = "";
   validchoice = false;
+  // Loop for so long as an invalid choice is made
   do {
     passwordlength = 0;
     passwordlength = prompt("Enter desired password length between 8 and 128");
     passwordlength = parseInt(passwordlength);
-    console.log(passwordlength);
+    // If password fits the criteria of length, set choice boolean to true to break the loop.
     if(passwordlength >= 8 && passwordlength <= 128){
       validchoice = true;
-      console.log("Check one");
     }
+    // If presses cancel, terminate the function.
     else if (!passwordlength){
-      console.log("Check terminate");
-      return;
+      return placeholder;
     }
     else {
-      console.log("Check two");
+      // Invalid length alert and shifts choice boolean to false.
       alert("You have entered an invalid password length, please try again.");
       validchoice = false;
     }
   } while(validchoice === false)
   do {
+    // True/false boolean confirm checks for each of the character typings.
   allowlowercase = confirm("Do you wish to include lowercases to your password? Confirm = yes, Cancel = no.");
   allowuppercase = confirm("Do you wish to include uppercases to your password? Confirm = yes, Cancel = no.");
   allownumbers = confirm("Do you wish to include numeric characters to your password? Confirm = yes, Cancel = no.");
   allowspecialchars = confirm("Do you wish to include special characters to your password? Confirm = yes, Cancel = no.");
-  console.log(allowlowercase);
-  console.log(allowuppercase);
-  console.log(allownumbers);
-  console.log(allowspecialchars);
+    // Validation to ensure at least one type of character was chosen.
     if(!allowlowercase && !allowuppercase && !allownumbers && !allowspecialchars){
       validchoice = false;
-      alert("Error, no useable characters selected, please try again.");
+      // Confirm on failed character input so that the user has the choice to terminate or try again.
+      let exit = confirm("Error, no useable characters selected, do you want to try again?");
+      if (!exit) {
+        return placeholder;
+      }
     }
+    // If any of the four character type booleans are true, set choice to true and break the loop.
     else {
       validchoice = true;
     }
   } while(validchoice === false)
+  // Boolean check each character type, if it was selected, add it to an array of valid characters.
   if (allowlowercase){
     chosencharacters.push(...passlowercase);
   }
@@ -87,15 +86,14 @@ function generatePassword(){
   if (allowspecialchars){
     chosencharacters.push(...passspecial);
   }
-  console.log(chosencharacters);
+  // Run a for loop based on the length of selected password, to grab randomly from an index determined on valid character array.
   for (i = 0; i < passwordlength; i++)
   {
     let x = Math.floor(Math.random() * (chosencharacters.length));
     newpassword = newpassword + chosencharacters[x];
   }
-  console.log(newpassword);
-  //newpassword = toString(newpassword);
-  //console.log(newpassword);
+
+  //End of function, return the randomly generated password.
   return newpassword;
 }
 
